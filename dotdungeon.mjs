c95a09aa6e238afab83c1d2e731b1d6c8f6e8788
Oliver-Akins <Oliver.akins1999@gmail.com>
2023-12-31 14:08:52 -0700

    Move the main module file into the repo root

--- dotdungeon.mjs
+++ dotdungeon.mjs
@@ -1,25 +1,25 @@
 // Data Models
-import { AspectItemData } from "./models/AspectItemData.mjs";
-import { PlayerData } from "./models/PlayerData.mjs";
-import { SyncData } from "./models/SyncData.mjs";
+import { AspectItemData } from "./module/models/AspectItemData.mjs";
+import { PlayerData } from "./module/models/PlayerData.mjs";
+import { SyncData } from "./module/models/SyncData.mjs";
 
 // Main Documents
-import { PlayerActor } from "./documents/PlayerActor.mjs";
-import { AspectItem } from "./documents/AspectItem.mjs";
+import { PlayerActor } from "./module/documents/PlayerActor.mjs";
+import { AspectItem } from "./module/documents/AspectItem.mjs";
 
 // Character Sheets
-import { AspectSheet } from "./sheets/AspectSheet.mjs";
-import { PlayerSheet } from "./sheets/PlayerSheet.mjs";
-import { BasicSyncSheet } from "./sheets/SyncVariations/BasicSyncSheet.mjs";
+import { AspectSheet } from "./module/sheets/AspectSheet.mjs";
+import { PlayerSheet } from "./module/sheets/PlayerSheet.mjs";
+import { BasicSyncSheet } from "./module/sheets/SyncVariations/BasicSyncSheet.mjs";
 
 // Utility imports
-import * as hbs from "./handlebars.mjs";
+import * as hbs from "./module/handlebars.mjs";
 
 // Non-Setup hooks
-import "./hooks/hotReload.mjs";
+import "./module/hooks/hotReload.mjs";
 
 // Misc Imports
-import loadSettings from "./settings/index.mjs";
+import loadSettings from "./module/settings/index.mjs";
 
 
 Hooks.once(`init`, () => {
