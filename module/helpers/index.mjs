f974f260b36b877e1129212f9272ee9f190e9bd9
Oliver-Akins <Oliver.akins1999@gmail.com>
2023-12-31 15:08:50 -0700

    Add logic helpers for sanity

--- module/helpers/index.mjs
+++ module/helpers/index.mjs
@@ -5,11 +5,25 @@ import { objectValue } from "./objectValue.mjs";
 import { toFriendlyDuration } from "./toFriendlyDuration.mjs";
 
 export default {
+
+	// Complex helpers
 	"dd-schemaOptions": schemaOptions,
 	"dd-array": createArray,
 	"dd-toFriendlyDuration": toFriendlyDuration,
 	"dd-objectValue": objectValue,
 	"dd-expanded": detailsExpanded,
+
+	// Simple helpers
 	"dd-stringify": v => JSON.stringify(v, null, `  `),
-	"dd-negate": v => !v,
+
+	// Logic helpers
+	"eq": (a, b) => a == b,
+	"neq": (a, b) => a != b,
+	"not": v => !v,
+	"or": (a, b) => a || b,
+	"and": (a, b) => a && b,
+	"nor": (a, b) => !(a || b),
+	"nand": (a, b) => !(a && b),
+	"xor": (a, b) => (a || b) && !(a && b),
+	"xnor": (a, b) => !((a || b) && !(a && b))
 };
