a3f5e23b7cd6c8b4281bd6d649be6e456e1bd435
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-01-08 23:17:18 -0700

    Add dice icons

--- module/handlebars.mjs
+++ module/handlebars.mjs
@@ -24,6 +24,12 @@ export const icons = [
 	`caret-right.svg`,
 	`garbage-bin.svg`,
 	`chat-bubble.svg`,
+	`dice/d4.svg`,
+	`dice/d6.svg`,
+	`dice/d8.svg`,
+	`dice/d10.svg`,
+	`dice/d12.svg`,
+	`dice/d20.svg`,
 ];
 
 
