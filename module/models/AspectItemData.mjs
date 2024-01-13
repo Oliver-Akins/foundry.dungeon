cb888bebadcf88459c3738d44d702f631f703d3e
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-01-13 15:06:02 -0700

    Tweak the Aspect data model

--- module/models/AspectItemData.mjs
+++ module/models/AspectItemData.mjs
@@ -2,7 +2,7 @@ export class AspectItemData extends foundry.abstract.DataModel {
 	static defineSchema() {
 		const fields = foundry.data.fields;
 		return {
-			used: new fields.BooleanField(),
+			used: new fields.BooleanField({ initial: false }),
 			/** The number of seconds that the effect of the aspect stays */
 			deactivateAfter: new fields.NumberField({ nullable: true }),
 			info: new fields.HTMLField({ nullable: true, blank: false, trim: true }),
