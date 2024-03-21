df381135334570a03215efbdd77663ff8d629d7d
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-03-20 21:20:51 -0600

    Simplify logic that I did weirdly for some reason

--- module/documents/Item/GenericItem.mjs
+++ module/documents/Item/GenericItem.mjs
@@ -2,10 +2,10 @@ export class DotDungeonItem extends Item {
 	get usedCapacity() {
 		let capacity = 0;
 		if (this.system.uses_inventory_slot && this.system.quantity > 0) {
-			capacity++;
+			capacity = 1;
 		};
 		if (this.system.quantity_affects_used_capacity) {
-			capacity *= this.system.quantity;
+			capacity = this.system.quantity;
 		};
 		return capacity;
 	};
