0605542d62414fdeeef1976325e4cfdd199229b7
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-03-21 21:24:37 -0600

    Remove console log

--- module/documents/Item/Material.mjs
+++ module/documents/Item/Material.mjs
@@ -3,7 +3,6 @@ import { DotDungeonItem } from "./GenericItem.mjs";
 export class Material extends DotDungeonItem {
 	get usedCapacity() {
 		let affects = game.settings.get(`dotdungeon`, `materialsAffectCapacity`);
-		console.log(`materialsAffectCapacity =`, affects)
 		return affects ? super.usedCapacity : 0;
 	};
 };
