1ba6cd06177381cfed3cd279f867b369a6e70b73
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-03-23 13:00:09 -0600

    Remove properties from the data model that aspects don't need

--- module/models/Item/Aspect.mjs
+++ module/models/Item/Aspect.mjs
@@ -3,10 +3,17 @@ import { DescribedItemData } from "./DescribedItemData.mjs";
 export class AspectItemData extends DescribedItemData {
 	static defineSchema() {
 		const fields = foundry.data.fields;
-		return {
+		const parentSchema = super.defineSchema();
+
+		// Purge fields that I don't want in this schema
+		delete parentSchema.quantity;
+		delete parentSchema.quantity_affects_used_capacity;
+		delete parentSchema.usage_cost;
+
+		return mergeObject(parentSchema, {
 			used: new fields.BooleanField({ initial: false }),
 			/** The number of seconds that the effect of the aspect stays */
 			deactivateAfter: new fields.NumberField({ nullable: true }),
-		};
+		});
 	};
 };
