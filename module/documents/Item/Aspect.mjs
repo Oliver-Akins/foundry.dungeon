649381cfdd7895057a55b93a8ae7dddcfbdc8d50
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-03-24 16:53:12 -0600

    Make the friendly duration be a getter on the Aspect class

--- module/documents/Item/Aspect.mjs
+++ module/documents/Item/Aspect.mjs
@@ -1,5 +1,8 @@
 import { DotDungeonItem } from "./GenericItem.mjs";
 
+const secondsInAMinute = 60;
+const secondsInAnHour = 60 * secondsInAMinute;
+
 export class Aspect extends DotDungeonItem {
 	async _preCreate() {
 		if (this.isEmbedded) {
@@ -16,5 +19,24 @@ export class Aspect extends DotDungeonItem {
 
 			return await this.actor?.preItemEmbed(this);
 		};
-	}
+	};
+
+	get friendlyDuration() {
+		let friendly = ``;
+		let duration = this.system.deactivateAfter;
+		if (duration >= secondsInAnHour) {
+			let hours = Math.floor(duration / secondsInAnHour);
+			friendly += `${hours}h`;
+			duration -= hours * secondsInAnHour;
+		};
+		if (duration >= secondsInAMinute) {
+			let minutes = Math.floor(duration / secondsInAMinute);
+			friendly += `${minutes}m`;
+			duration -= minutes * secondsInAMinute;
+		};
+		if (duration > 0) {
+			friendly += `${duration}s`;
+		};
+		return friendly;
+	};
 };
