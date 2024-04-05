d8b676535f98620bdea1016c44f93443e58990f5
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-04-04 23:14:46 -0600

    Add incrementer button support for item sheets

--- module/sheets/Items/GenericItemSheet.mjs
+++ module/sheets/Items/GenericItemSheet.mjs
@@ -10,13 +10,6 @@ export class GenericItemSheet extends ItemSheet {
 		`resourcesOrSupplies`,
 	];
 
-	activateListeners(html) {
-		super.activateListeners(html);
-
-		if (!this.isEditable) return;
-		console.debug(`.dungeon | Adding event listeners for Generic Item: ${this.id}`);
-	};
-
 	async getData() {
 		const ctx = {};
 
@@ -41,5 +34,36 @@ export class GenericItemSheet extends ItemSheet {
 		ctx.icons = CONFIG.CACHE.icons;
 
 		return ctx;
+	};
+
+	activateListeners(html) {
+		super.activateListeners(html);
+
+		if (!this.isEditable) return;
+		console.debug(`.dungeon | Adding event listeners for Generic Item: ${this.id}`);
+		html.find(`button[data-increment]`)
+			.on(`click`, this._incrementValue.bind(this));
+		html.find(`button[data-decrement]`)
+			.on(`click`, this._decrementValue.bind(this));
+	};
+
+	async _incrementValue($e) {
+		const target = $e.currentTarget;
+		const data = target.dataset;
+		const value = getProperty(this.actor, data.increment);
+		if (typeof value != "number") {
+			return;
+		};
+		this.actor.update({ [data.increment]: value + 1 });
+	};
+
+	async _decrementValue($e) {
+		const target = $e.currentTarget;
+		const data = target.dataset;
+		const value = getProperty(this.actor, data.decrement);
+		if (typeof value != "number") {
+			return;
+		};
+		this.actor.update({ [data.decrement]: value - 1 });
 	};
 };
